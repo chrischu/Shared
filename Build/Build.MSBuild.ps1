0f6fcedef691d36bd9ea2405e3edb7a9d5252f22
Christian Schuster <christian.schuster@live.com>
2016-07-02 14:14:32 +0200

    Added functionality to apply XDT transforms via MSBuild.

--- Build/Build.MSBuild.ps1
+++ Build/Build.MSBuild.ps1
@@ -47,3 +47,31 @@ BuildStep Package-Projects {
     Exec { & $MSBuildExecutable "$($project.ProjectPath)" /t:Package /m /nr:False "/p:Configuration=$configuration;SolutionDir=$solutionDirectory" } -ErrorMessage "Could not package project '$($project.ProjectName)'"
   }
 }
+
+BuildStep Apply-XdtTransform {
+  Param(
+    [Parameter(Mandatory)] [Project] $project,
+    [Parameter(Mandatory)] [string] $file,
+    [Parameter(Mandatory)] [string] $transform
+  )
+
+  try {
+    $transformProject = Join-Path $project.ProjectDir "Transform.proj"
+    Write-Host "TFP: $transformProject"
+    $transformContent = @"
+<Project ToolsVersion="$MSBuildToolset" DefaultTargets="Transform" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <UsingTask TaskName="TransformXml" AssemblyFile="`$(MSBuildExtensionsPath)\Microsoft\VisualStudio\v$MSBuildToolset\Web\Microsoft.Web.Publishing.Tasks.dll"/>
+  <Target Name="Transform">
+    <TransformXml Source="$file" Transform="$transform" Destination="$file"/>
+  </Target>
+</Project>
+"@
+    
+    Set-Content -Path $transformProject -Value $transformContent
+
+    Exec { & $MSBuildExecutable $transformProject /t:Transform /m /nr:False } -ErrorMessage "Could not transform file '$file'"
+
+  } finally {
+    Remove-Item $transformProject
+  }
+}
